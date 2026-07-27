c6cf4ee8f2877869938d0d2c0675fc96de6a926b
Arjuna-21 <tchinna596@gmail.com>
2026-07-27 22:56:32 +0530

    Added default route

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login"
 import Register from "./pages/Register"
 import Dashboard from "./pages/Dashboard"
+import { useNavigate } from 'react-router-dom';
 import "./index.css";
 
 function App() {
   return (
     <div>
         <Routes>
+          <Route path="/" element={<Navagate to ="/login" replace />} />
           <Route path='/login' element={<Login />} />
           <Route path='/register' element={<Register />} />
           <Route path='/dashboard' element={<Dashboard />} />
